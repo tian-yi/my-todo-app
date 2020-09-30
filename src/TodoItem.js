ba8291833eb8b298e2aa03cd26f2fc8bdd5db911
Tianyi Wang <t@tianyi.dev>
2020-09-30 12:10:08 +0100

    Convert the ToDoItem to use JSX

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const TodoItem = ({id, labelName}) => {
-  return  React.createElement(
-    'div', 
-    {className: "todo-item"}, 
-    [
-      React.createElement("input", {type: "checkbox", id: id}),
-      React.createElement("label", {htmlFor: id}, labelName)
-    ]
+  return (
+  <div className="todo-item">
+    <input type="checkbox" id={id}/>
+    <label htmlFor={id}>
+      {labelName}
+    </label>
+  </div>
   )
 }
 
