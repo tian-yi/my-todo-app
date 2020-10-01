4a3decfc0e1f23d40d42a56cfd9e7d7bc67fd4d0
Tianyi Wang <t@tianyi.dev>
2020-10-01 12:14:04 +0100

    Add todo states by using React useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 import TodoItem from "./TodoItem";
@@ -6,18 +6,43 @@ import TodoItem from "./TodoItem";
 const TodoItemsFromOutSide = [
   { id: "learn-react", labelName: "Learn React" },
   { id: "create-todo-app", labelName: "Create a todo app" },
-  { id: "profit", labelName: "Profitx" },
+  { id: "profit", labelName: "Profit" },
   { id: "have-fun", labelName: "Have fun!" },
   { id: "prettier", labelName: "Prettier is awesome!" },
 ];
 
-const App = () => (
-  <div id="my-todo-app" className="my-todo-app">
-    <h1>My todo app</h1>
-    {TodoItemsFromOutSide.map((item) => {
-      return <TodoItem key={item.id} id={item.id} labelName={item.labelName} />;
-    })}
-  </div>
-);
+const App = () => {
+  const [todos, setTodos] = useState(TodoItemsFromOutSide);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodos = [
+      ...todos,
+      { id: newTodo.replace(" ", "-"), labelName: newTodo },
+    ];
+    setTodos(newTodos);
+    setNewTodo("");
+  };
+
+  return (
+    <div id="my-todo-app" className="my-todo-app">
+      <h1>My todo app</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          value={newTodo}
+          onChange={(e) => {
+            setNewTodo(e.target.value);
+          }}
+        />
+      </form>
+      {todos.map((item) => {
+        return (
+          <TodoItem key={item.id} id={item.id} labelName={item.labelName} />
+        );
+      })}
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
