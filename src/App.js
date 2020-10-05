7881c34ca38076f1f44797dd04a7df6e49645981
Tianyi Wang <t@tianyi.dev>
2020-10-05 22:21:35 +0100

    Lesson three

--- src/App.js
+++ src/App.js
@@ -1,46 +1,80 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
+import "./style.css";
 import TodoItem from "./TodoItem";
 
-const TodoItemsFromOutSide = [
-  { id: "learn-react", labelName: "Learn React" },
-  { id: "create-todo-app", labelName: "Create a todo app" },
-  { id: "profit", labelName: "Profit" },
-  { id: "have-fun", labelName: "Have fun!" },
-  { id: "prettier", labelName: "Prettier is awesome!" },
-];
+// const TodoItemsFromOutSide = [
+//   { id: "learn-react", labelName: "Learn React" },
+//   { id: "create-todo-app", labelName: "Create a todo app" },
+//   { id: "profit", labelName: "Profit" },
+//   { id: "have-fun", labelName: "Have fun!" },
+//   { id: "prettier", labelName: "Prettier is awesome!" },
+// ];
 
 const App = () => {
-  const [todos, setTodos] = useState(TodoItemsFromOutSide);
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newTodos = [
       ...todos,
-      { id: newTodo.replace(" ", "-"), labelName: newTodo },
+      { id: newTodo.replace(" ", "-"), labelName: newTodo, completed: false },
     ];
     setTodos(newTodos);
     setNewTodo("");
   };
 
+  useEffect(() => {
+    const todos = localStorage.getItem("todos");
+    setTodos(JSON.parse(todos));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const updateTodo = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  };
   return (
-    <div id="my-todo-app" className="my-todo-app">
-      <h1>My todo app</h1>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input
-          value={newTodo}
-          onChange={(e) => {
-            setNewTodo(e.target.value);
-          }}
-        />
-      </form>
-      {todos.map((item) => {
-        return (
-          <TodoItem key={item.id} id={item.id} labelName={item.labelName} />
-        );
-      })}
+    <div className="todoapp">
+      <header className="header">
+        <h1>Todos</h1>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            placeholder="What needs to be done?"
+            className="new-todo"
+            value={newTodo}
+            onChange={(e) => {
+              setNewTodo(e.target.value);
+            }}
+          />
+        </form>
+      </header>
+      <section className="main">
+        <ul className="todo-list">
+          {todos.map((item) => {
+            return (
+              <TodoItem
+                handleChange={updateTodo}
+                key={item.id}
+                id={item.id}
+                labelName={item.labelName}
+                completed={item.completed}
+              />
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 };
